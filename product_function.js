dd48c15ce8443c7c8feabee318cb0b5b7cd85cfd
Ihor Artomov <igora2406@gmail.com>
2023-05-18 18:41:30 +0300

    Add comments

--- product_function.js
+++ product_function.js
@@ -1,3 +1,15 @@
+"use strict";
+
+/**
+ * Constructor for Product object.
+ * @param {string} ID - The product key (unique value).
+ * @param {string} name - The product's name.
+ * @param {string} brand - The product's brand.
+ * @param {number} price - The product's price.
+ * @param {string} activeSize - The product's active size.
+ * @param {number} quantity - Number of products in stock.
+ */
+
 function Product(ID, name, brand, price, activeSize, quantity) {
   this.ID = ID;
   this.name = name;
@@ -118,20 +130,17 @@ function Product(ID, name, brand, price, activeSize, quantity) {
    */
   this.getAverageRating = function () {
     let rating = 0;
-    if (this.reviews.length === 0) {
-      return rating;
-    }
     this.reviews.forEach((review) => {
       rating += sumObjectProperties(review.rating);
     });
-    return rating / this.reviews.length;
+    return this.reviews.length === 0 ? rating : rating / this.reviews.length;
   };
   /**
    * Returns the product rating from one review.
    * @param {Object} object - an object containing the values of the product rating properties.
    * @returns The product rating from one review.
    */
-  sumObjectProperties = function (object) {
+  function sumObjectProperties(object) {
     let sum = { sum: 0, properties: 0 };
     for (let property in object) {
       if (
@@ -142,13 +151,20 @@ function Product(ID, name, brand, price, activeSize, quantity) {
         sum.properties++;
       }
     }
-    if (sum.properties === 0) {
-      return sum.sum;
-    }
-    return sum.sum / sum.properties;
-  };
+    return sum.properties === 0 ? sum.sum : sum.sum / sum.properties;
+  }
 }).call(Product.prototype);
 
+/**
+ * Constructor for Review object.
+ * @param {string} ID - the review key (unique value).
+ * @param {string} author - the review author name.
+ * @param {string} comment - the comment for the product.
+ * @param {number} service - the product service rating.
+ * @param {number} price - the product price rating.
+ * @param {number} value - the product value rating.
+ * @param {number} quality - the product quality rating.
+ */
 function Review(ID, author, comment, service, price, value, quality) {
   this.ID = ID;
   this.author = author;
@@ -163,7 +179,7 @@ function Review(ID, author, comment, service, price, value, quality) {
 }
 
 /**
- * Implements the search for products by matching the search query 
+ * Implements the search for products by matching the search query
  * in the product name or description
  * @param {Array} products - array of Product objects.
  * @param {string} search - the desired match.
@@ -186,8 +202,7 @@ function search(products, search) {
  * @returns True if text contains search.
  */
 function isMatch(property, search) {
-  search = new RegExp(`${search}`, "i");
-  return search.test(property);
+  return new RegExp(`\\b${search}`, "i").test(property);
 }
 
 /**
@@ -197,21 +212,82 @@ function isMatch(property, search) {
  * @returns Sorting array of products by sortRule.
  */
 function sort(products, sortRule) {
-  const sortProducts = Object.assign({}, products);
-  switch (sortRule) {
-    case "iDUp":
-      return sortProducts.sort((a, b) => a.id.localeCompare(b.id));
-    case "iDDown":
-      return sortProducts.sort((a, b) => b.id.localeCompare(a.id));
-    case "priceUp":
+  const sortProducts = products.slice();
+  switch (sortRule.toLowerCase()) {
+    case "idup":
+      return sortProducts.sort((a, b) => a.ID.localeCompare(b.ID));
+    case "iddown":
+      return sortProducts.sort((a, b) => b.ID.localeCompare(a.ID));
+    case "priceup":
       return sortProducts.sort((a, b) => a.price - b.price);
-    case "priceDown":
+    case "pricedown":
       return sortProducts.sort((a, b) => b.price - a.price);
-    case "nameUp":
+    case "nameup":
       return sortProducts.sort((a, b) => a.name.localeCompare(b.name));
-    case "nameDown":
+    case "namedown":
       return sortProducts.sort((a, b) => b.name.localeCompare(a.name));
     default:
       return products;
   }
 }
+
+const product1 = new Product("p1", "T-shirt", "Adidas", 12, "M", 5);
+const product2 = new Product("p2", "Tshirt", "Puma", 11, "XL", 8);
+const product3 = new Product("p3", "Skirt", "Cos", 15, "M", 3);
+const product4 = new Product("p4", "Shorts", "Nike", 14, "XS", 1);
+const product5 = new Product("p5", "Pants", "Reebok", 25, "L", 2);
+
+const review1 = new Review("r1", "Max", "Super", 5, 5, 5, 5);
+const review2 = new Review("r2", "Alex", "Good", 4, 4, 4, 4);
+const review3 = new Review("r3", "Serg", "Norm", 3, 3, 3, 3);
+const review4 = new Review("r4", "Max", "OK", 5, 4, 4, 5);
+const review5 = new Review("r5", "Andrew", "Bad", 2, 2, 2, 2);
+
+const reviews = [];
+reviews.push(review1);
+reviews.push(review2);
+reviews.push(review3);
+reviews.push(review4);
+reviews.push(review5);
+
+product1.setReviews(reviews);
+product2.setReviews(reviews);
+product3.setReviews(reviews);
+product4.setReviews(reviews);
+product5.setReviews(reviews);
+
+const products = [product1, product2, product3, product4, product5];
+const sortRules = [
+  "idup",
+  "iddown",
+  "priceup",
+  "Pricedown",
+  "NamEuP",
+  "nAmedoWn",
+];
+for (let sortRule of sortRules) {
+  let sortProducts = sort(products, sortRule);
+  console.log(sortRule)
+  sortProducts.forEach((product) => {
+    switch (sortRule) {
+      case "idup":
+        console.log(product.ID);
+        break;
+      case "iddown":
+        console.log(product.ID);
+        break;
+      case "priceup":
+        console.log(product.price);
+        break;
+      case "Pricedown":
+        console.log(product.price);
+        break;
+      case "NamEuP":
+        console.log(product.name);
+        break;
+      case "nAmedoWn":
+        console.log(product.name);
+    }
+  });
+}
+
